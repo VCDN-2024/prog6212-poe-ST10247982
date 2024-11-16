8ace29b62441a7b561406907e04d85a6cbf95378
Shravan Ramjathan <128147436+ST10247982@users.noreply.github.com>
2024-11-16 20:24:00 +0200

    Added CourseValidation

---
+++ POE-p2-s4/Validation/CourseValidation.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using POE_p2_s4.Models;
+
+namespace POE_p2_s4.Validation
+{
+    public class CourseValidation: AbstractValidator<Course>
+    {
+        public CourseValidation()
+        {
+           
+            RuleFor(course => course.Name).Length(3, 50).NotNull().WithMessage("Please enter a {PropertyName} name with 3-50 character");
+            RuleFor(course => course.Department).Length(3, 50).NotNull().WithMessage("Please enter a {PropertyName} name with {Minlength} - {MaxLength} character");
+            RuleFor(course => course.UserId).Length(255).NotNull().WithMessage("No {PropertyName} associated with this course. ");
+        }
+    }
+}
