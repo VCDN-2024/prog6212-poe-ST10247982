732eed2ab382282e8aeddf4a174550f1f03cbeea
Shravan Ramjathan <128147436+ST10247982@users.noreply.github.com>
2024-11-16 17:24:03 +0200

    Added Course ViewModel

---
+++ POE-p2-s4/ViewModels/Course.cs
@@ -0,0 +1,13 @@
+﻿using POE_p2_s4.Models;
+
+namespace POE_p2_s4.ViewModels
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string UserId { get; set; }
+
+    }
+}
