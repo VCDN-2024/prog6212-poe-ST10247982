8bdb34b5c079d7548a9624d5d7d359b1d3c9c312
Shravan Ramjathan <128147436+ST10247982@users.noreply.github.com>
2024-11-16 17:22:43 +0200

    Added Claim ViewModel

---
+++ POE-p2-s4/ViewModels/Claim.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace POE_p2_s4.ViewModels
+{
+    public class Claim
+    {
+        public string ClaimType { get; set; }
+        public string Description { get; set; }
+        public DateTime ClaimDate { get; set; }
+        public double HoursWorked { get; set; }
+        public double? ClaimExpenses { get; set; }
+
+        public byte[]? DocumentBinary { get; set; }
+ 
+        public IFormFile? Document { get; set; }
+        public string ClaimStatus { get; set; }
+        public string UserId { get; set; }
+    }
+}
