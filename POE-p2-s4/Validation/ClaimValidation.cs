bca971e9c989a0d34853045bd6379f1d681e8dd2
Shravan Ramjathan <128147436+ST10247982@users.noreply.github.com>
2024-11-21 20:11:40 +0200

    Updated ClaimValidation -> New calculation

--- POE-p2-s4/Validation/ClaimValidation.cs
+++ POE-p2-s4/Validation/ClaimValidation.cs
@@ -12,7 +12,7 @@ namespace POE_p2_s4.Validation
 
      
         private readonly decimal _foodExpenseMultiplier = 0.4m;  
-        private readonly decimal _petrolExpenseMultiplier = 0.8m;
+ 
         
         public ClaimValidation(decimal HourlYRate)
         {
@@ -22,9 +22,11 @@ namespace POE_p2_s4.Validation
             RuleFor(claim => claim.ClaimType).Length(3, 50).NotNull().WithMessage("Please enter a {PropertyName} name with {Minlength} - {MaxLength} character");
             RuleFor(claim => claim.ClaimDate).NotNull().WithMessage("Please enter a valid date");
             RuleFor(claim => claim.HoursWorked).GreaterThan(0).LessThan(40).NotNull().WithMessage("Please enter a valid work hour amount ");
+            RuleFor(claim => claim.KilometersTravelled).GreaterThan(0).WithMessage("Please enter a distance greater than 0");
             RuleFor(claim => claim)
                .Must(ValidateClaimAmount)
                .WithMessage(claim => $"The {claim.ClaimType} expense exceeds the allowable limit based on hours worked and hourly rate.");
+           
 
         }
         private bool ValidateClaimAmount(Claim claim)
@@ -44,7 +46,7 @@ namespace POE_p2_s4.Validation
                     return claim.ClaimExpenses <=(double) (maxAllowableExpense * _foodExpenseMultiplier);
 
                 case "Travel":
-                    return claim.ClaimExpenses <=(double) (maxAllowableExpense * _petrolExpenseMultiplier);
+                    return claim.ClaimExpenses <=(double) (maxAllowableExpense *  claim.KilometersTravelled);
                 case "Leave":
                     return claim.ClaimExpenses <= (double)(maxAllowableExpense);
                 
