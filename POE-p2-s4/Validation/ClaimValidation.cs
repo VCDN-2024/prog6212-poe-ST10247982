1bd162f4cb8a76bcf0bda0ed618491ba72285d57
Shravan Ramjathan <128147436+ST10247982@users.noreply.github.com>
2024-11-21 14:03:43 +0200

    Added Claim Validation

---
+++ POE-p2-s4/Validation/ClaimValidation.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation;
+using POE_p2_s4.Models;
+using POE_p2_s4.ViewModels;
+using System.Security.Claims;
+using Claim = POE_p2_s4.Models.Claim;
+
+namespace POE_p2_s4.Validation
+{
+    public class ClaimValidation:AbstractValidator<Claim>
+    {
+        private readonly decimal _hourlyRate;
+
+     
+        private readonly decimal _foodExpenseMultiplier = 0.2m;  
+        private readonly decimal _petrolExpenseMultiplier = 0.4m;
+        public ClaimValidation(decimal HourlYRate)
+        {
+            _hourlyRate = HourlYRate;
+            RuleFor(claim => claim.ClaimType).Length(3, 50).NotNull().WithMessage("Please enter a {PropertyName} name with {Minlength} - {MaxLength} character");
+            RuleFor(claim => claim.Description).Length(3, 50).NotNull().WithMessage("Please enter a {PropertyName} name with {Minlength} - {MaxLength} character");
+            RuleFor(claim => claim.ClaimType).Length(3, 50).NotNull().WithMessage("Please enter a {PropertyName} name with {Minlength} - {MaxLength} character");
+            RuleFor(claim => claim.ClaimDate).NotNull().WithMessage("Please enter a valid date");
+            RuleFor(claim => claim.HoursWorked).GreaterThan(0).LessThan(40).NotNull().WithMessage("Please enter a valid work hour amount ");
+            RuleFor(claim => claim)
+               .Must(ValidateClaimAmount)
+               .WithMessage(claim => $"The {claim.ClaimType} expense exceeds the allowable limit based on hours worked and hourly rate.");
+
+        }
+        private bool ValidateClaimAmount(Claim claim)
+        {
+            if (claim.HoursWorked <= 0)
+            {
+                
+                return true;
+            }
+
+          
+            decimal maxAllowableExpense = (decimal)claim.HoursWorked * _hourlyRate;
+
+            switch (claim.ClaimType)
+            {
+                case "Food":
+                    return claim.ClaimExpenses <=(double) (maxAllowableExpense * _foodExpenseMultiplier);
+
+                case "Petrol":
+                    return claim.ClaimExpenses <=(double) (maxAllowableExpense * _petrolExpenseMultiplier);
+
+                default:
+                    
+                    return false;
+            }
+        }
+}
+      
+}
