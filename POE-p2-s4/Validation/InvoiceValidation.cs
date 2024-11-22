be50126ea75eeed974baeb4b6fe838a9460a6134
Shravan Ramjathan <128147436+ST10247982@users.noreply.github.com>
2024-11-22 17:30:02 +0200

    Added Invoice Validation model

---
+++ POE-p2-s4/Validation/InvoiceValidation.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using POE_p2_s4.Models;
+
+namespace POE_p2_s4.Validation
+{
+    public class InvoiceValidation:AbstractValidator<Invoice>
+    {
+        public InvoiceValidation() 
+        {
+
+            RuleFor(invoice => invoice.Id).Length(33, 50).NotNull().WithMessage("Please enter a {PropertyName}  with {Minlength} - {MaxLength} character");
+            RuleFor(invoice => invoice.Title).Length(4, 30).NotNull().WithMessage("Please enter a {PropertyName} name with {Minlength} - {MaxLength} character");
+            RuleFor(invoice => invoice.UserId).Length(33, 50).NotNull().WithMessage("No {PropertyName} associated with this course. ");
+            RuleFor(invoice => invoice.IsPaymentMade).NotNull().WithMessage("No {PropertyName} specification made! ");
+            RuleFor(invoice => invoice.Claims).NotEmpty().WithMessage("Not a single claim in the list of {PropertyName}!");
+            RuleFor(invoice => invoice.CreatedDate).NotNull().WithMessage("There is no valid {Propertyname} for this invoice!");
+        }
+    }
+}
